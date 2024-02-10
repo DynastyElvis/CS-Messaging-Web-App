b14d1df94ef2970764e65ed6cb61745347826798
Elvis Kipkemoi <elvis@Elviss-MacBook-Pro.local>
2024-02-10 18:35:15 +0300

    ft:agents chats

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -14,6 +14,31 @@ router.route("/:id").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+router.route("/login/:id").post((req, res) => {
+  User.findOne({ id: req.params.id })
+    .then((user) => {
+      if (user) return res.send(user);
 
+      const id = req.params.id;
+      const newChat = new Chat({ id, assigned: false });
+      newChat
+        .save()
+        .then((chat) => {
+          const newUser = new User({
+            id: req.params.id,
+            chatId: chat.id,
+          });
+
+          newUser
+            .save()
+            .then((user) => {
+              res.json(user);
+            })
+            .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
 module.exports = router;
