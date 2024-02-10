5fee91ff188f0c84c133827cf58a1bddb22832aa
Elvis Kipkemoi <elvis@Elviss-MacBook-Pro.local>
2024-02-10 12:22:44 +0300

    ft: page auth logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,13 @@ import AuthPage from "./AuthPage";
 import ChatsPage from "./ChatsPage";
 
 function App() {
+  const [user, setUser] = useState(undefined);
 
+  if (!user) {
+    return <AuthPage onAuth={(user) => setUser(user)} />;
+  } else {
+    return <ChatsPage user={user} />;
+  }
+}
 
 export default App;
