0d119dc17c131664fe7ab6f2cdade5601e0a4ce1
Elvis Kipkemoi <elvis@Elviss-MacBook-Pro.local>
2024-02-10 11:05:02 +0300

    ft: chat engine API

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,22 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
+
+  try{
+    const r = await axios.put(
+    'https://api.chatengine.io/users/'
+    {username: username, secret: username, first_name: username},
+    {headers: {"private-key": "689edb14-1128-43dc-b901-46b07dc796ac"}}
+    )
+    return res.status(r.status)
+  } catch (e){
+    axios
+  }
+  
+
+
   return res.json({ username: username, secret: "sha256..." });
 });
+
 
 app.listen(3001);
