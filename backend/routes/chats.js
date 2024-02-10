60405bf14942bf0e96cc6fbffa87bd6486b2ad01
Elvis Kipkemoi <elvis@Elviss-MacBook-Pro.local>
2024-02-10 18:33:59 +0300

    ft:agents chats

--- backend/routes/chats.js
+++ backend/routes/chats.js
@@ -9,3 +9,48 @@ const chatsRouter = (io) => {
       .then((users) => res.json(users))
       .catch((err) => res.status(400).json("Error: " + err));
   });
+
+  router.route("/:id").get((req, res) => {
+    Chat.findOne({ id: req.params.id })
+      .then((user) => res.json(user))
+      .catch((err) => res.status(400).json("Error: " + err));
+  });
+
+  router.route("/assign/:id").post((req, res) => {
+    Chat.findOneAndUpdate(
+      { id: req.params.id },
+      { assigned: true },
+      { returnOriginal: false }
+    ).catch((err) => res.status(400).json("Error: " + err));
+
+    Agent.findOne({ id: req.body.id })
+      .then((agent) => {
+        if (agent.assignedChats.includes(req.params.id))
+          return res.json("already assigned");
+
+        const newAssignedChats = [...agent.assignedChats, req.params.id];
+
+        Agent.findOneAndUpdate(
+          { id: req.body.id },
+          { assignedChats: newAssignedChats },
+          { returnOriginal: true }
+        )
+          .then(() => res.json("assigned"))
+          .catch((err) => res.status(400).json("Error: " + err));
+      })
+      .catch((err) => res.status(400).json("Error: " + err));
+  });
+
+  
+
+    newMessage
+      .save()
+      .then(() => res.json("Message added!"))
+      .catch((err) => res.status(400).json("Error: " + err));
+
+    io.emit("new_message", newMessage);
+    //io.to(parseInt(chatId)).emit("new_message", newMessage);
+  });
+  return router;
+};
+module.exports = chatsRouter;
