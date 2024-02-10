875b5d3efc712184eb462aba52883f1262954678
Elvis Kipkemoi <elvis@Elviss-MacBook-Pro.local>
2024-02-10 12:10:20 +0300

    ft: json responses

--- backend/index.js
+++ backend/index.js
@@ -10,19 +10,15 @@ app.post("/authenticate", async (req, res) => {
 
   try{
     const r = await axios.put(
-    'https://api.chatengine.io/users/'
-    {username: username, secret: username, first_name: username},
-    {headers: {"private-key": "689edb14-1128-43dc-b901-46b07dc796ac"}}
-    )
-    return res.status(r.status)
-  } catch (e){
-    axios
+    "https://api.chatengine.io/users/",
+    { username: username, secret: username, first_name: username},
+    { headers: {"private-key": "689edb14-1128-43dc-b901-46b07dc796ac"}}
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
   }
-  
 
-
-  return res.json({ username: username, secret: "sha256..." });
+//   return res.json({ username: username, secret: "sha256..." });
 });
-
-
 app.listen(3001);
