131e869e10f1f52c47b2bf7484a35d3da4e6ce94
Elvis Kipkemoi <elvis@Elviss-MacBook-Pro.local>
2024-02-10 13:08:36 +0300

    fix: chat window bug

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -2,14 +2,14 @@ import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 const ChatsPage = (props) => {
   return (
-    <div className="background">
+    <div style={{ height: '100vh' }}>
+        return ()
       <PrettyChatWindow
         projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
         username={props.user.username}
         secret={props.user.secret}
       />
     </div>
-  );
-};
-
+  )
+}
 export default ChatsPage;
